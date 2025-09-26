cf07380c5a0e464b67710535608c65d57916f331
ConstantPy <DubstepCop@gmail.com>
2025-09-26 16:37:49 -0400

    Update input.py

--- input.py
+++ input.py
@@ -1,4 +1,9 @@
 keys_down = set()
+mouse_buttons_down = set()
+import pygame
 
 def is_key_pressed(key):
     return key in keys_down
+
+def is_mouse_pressed(button):
+    return pygame.mouse.get_pressed()[button]
