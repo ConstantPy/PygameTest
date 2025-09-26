c1471457e32a537d4e2b9a3b51e957ada2b4fcd8
ConstantPy <DubstepCop@gmail.com>
2025-09-26 16:34:23 -0400

    Update camera.py

--- camera.py
+++ camera.py
@@ -2,13 +2,17 @@ import pygame
 
 camera = pygame.Rect(0, 0, 0, 0)
 
-def create_screen(width, height, title):
-    screen = pygame.display.set_mode((width, height))
+def create_screen(title):
+    width = 1280
+    height = 720
+
     pygame.display.set_caption(title)
-    pygame_icon = pygame.image.load('images/DJIcon.png')
+
+    screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+
+    pygame_icon = pygame.image.load('content/images/DJIcon.png')
     pygame.display.set_icon(pygame_icon)
 
     camera.width = width
     camera.height = height
-
     return screen
